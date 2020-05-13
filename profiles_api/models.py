fdde4e877c3c3f1f744bc412a2aefba4f7bf1e5e
Iain <iaingrant18@gmail.com>
2020-05-13 18:18:12 +0100

    created super user login

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -12,7 +12,7 @@ class UserProfileManager(BaseUserManager):
         if not email:
             raise ValueError("Users Must have Email Address")
 
-        email = self.normilize_email(email)
+        email = self.normalize_email(email)
         user = self.model(email=email, name=name)
 
         user.set_password(password)
@@ -20,7 +20,7 @@ class UserProfileManager(BaseUserManager):
 
         return user
 
-    def create_super_user(self, email, name, password):
+    def create_superuser(self, email, name, password):
         """create  a super user with details"""
         user = self.create_user(email, name, password)
 
@@ -41,7 +41,7 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     objects = UserProfileManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELD = ['name']
+    REQUIRED_FIELDS = ['name']
 
     def get_full_name(self):
         """retrieve full name of user"""
