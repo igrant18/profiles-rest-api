bb3cbea558c42a8a4870f4d1f2c7f944cb85bc3e
Iain <iaingrant18@gmail.com>
2020-05-15 14:57:03 +0100

    added api view

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    def get(self, request, format=None):
+        an_apiview = [
+            'uses HTTP request to get resltd',
+            'GET, POST, DELETE, UPDATE, PATCH'
+        ]
+
+        return Response({'message': 'Hello', 'an_apiView': an_apiview})
